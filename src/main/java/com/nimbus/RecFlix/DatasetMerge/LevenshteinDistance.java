a4f345f461e89d7ed2082e5a9a05709a2176863d
Prabal Kanodia <prabalkanodia@gmail.com>
2013-10-13 19:00:54 -0400

    Comments

--- src/main/java/com/nimbus/RecFlix/DatasetMerge/LevenshteinDistance.java
+++ src/main/java/com/nimbus/RecFlix/DatasetMerge/LevenshteinDistance.java
@@ -16,17 +16,23 @@ public class LevenshteinDistance {
 		
 		int[][] dist = new int[s.length + 1][t.length + 1];
 		
+		// Source string can be modified to empty string by dropping all characters
 		for (int i = 0; i <= s.length; i++) {
 			dist[i][0] = i;
 		}
 		
+		// Target string could be formed from empty source string by inserting
+		// character by character
 		for (int j = 1; j <= t.length; j++) {
 			dist[0][j] = j;
 		}
 		
+		// If the source character at i matches with target character at j
+		//    then the cost is same as the previous operation (above-left) i.e., i-1 and j-1
+		// Else calculate min of elements above + 1, left + 1, above-left (diagonal) + 1
 		for (int i = 0; i <= s.length; i++) {
 			for (int j = 0; j <= t.length; j++) {
-				dist[i][j] = s[i] == t[j] 
+				dist[i][j] = (s[i] == t[j]) 
 					? dist[i - 1][j - 1] 
 					: min(
 						dist[i - 1][j] + 1, 
@@ -35,6 +41,8 @@ public class LevenshteinDistance {
 			}
 		}
 		
+		// The bottom and right most cell contains the net distance
+		// to transform one string to another
 		return dist[s.length][t.length];
 	}
 

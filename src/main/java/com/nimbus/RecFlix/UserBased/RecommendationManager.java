eadb0febdedd8d17d4e3177eb728e7b70f683f79
siva <siva_march21@yahoo.com>
2013-10-13 23:40:43 -0400

    first version of kuserneighborhood and recommendationmanager

--- src/main/java/com/nimbus/RecFlix/UserBased/RecommendationManager.java
+++ src/main/java/com/nimbus/RecFlix/UserBased/RecommendationManager.java
@@ -1,10 +1,19 @@
 package com.nimbus.RecFlix.UserBased;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecommendationManager {
 
 	/**
 	 * @param args
 	 * @author Siva
 	 */
+	
+	public List<MovieRating> getRecommendation(UserProfile user){
+		List<MovieRating> movies = new ArrayList<MovieRating>();
+		//todo
+		return movies;
+	}
 
 }

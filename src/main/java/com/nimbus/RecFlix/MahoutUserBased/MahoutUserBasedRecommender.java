9519becb6b30b22e448f595bbe9c5cd7837ceb64
divya <divyapinki@gmail.com>
2013-10-14 00:35:08 -0400

    adding mahout version of userbased recommender

---
+++ src/main/java/com/nimbus/RecFlix/MahoutUserBased/MahoutUserBasedRecommender.java
@@ -0,0 +1,13 @@
+package com.nimbus.RecFlix.MahoutUserBased;
+
+public class MahoutUserBasedRecommender {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

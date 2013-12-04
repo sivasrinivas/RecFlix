e017139054744a8f4b0381b7f8ca8a610e8c54cc
prabalkanodia <prabalkanodia@gmail.com>
2013-12-04 16:22:51 -0500

    merge utility to process imdb local dataset

--- src/main/java/com/nimbus/RecFlix/DatasetMerge/MergeUtility.java
+++ src/main/java/com/nimbus/RecFlix/DatasetMerge/MergeUtility.java
@@ -3,13 +3,18 @@
  */
 package com.nimbus.RecFlix.DatasetMerge;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.util.*;
 
+import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -21,165 +26,118 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
  */
 public class MergeUtility {
 	
-	//private Text word = new Text();
+	// private IntWritable mvID = new IntWritable();
+
+	// movie-id - movie-title pair
+	public static HashMap<Integer, String> movieCache = new HashMap<Integer, String>();
+	// movie-id - year pair
+	public static HashMap<Integer, Integer> yearCache = new HashMap<Integer, Integer>();
+
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+
+		Configuration conf = context.getConfiguration();
+		Path[] paths = DistributedCache.getLocalCacheFiles(conf);
+
+		for (Path file : paths) {
+			readCache(file);
+		}
+	}
+
+	private void readCache(Path path) throws IOException {
+		BufferedReader fis = new BufferedReader(new FileReader(
+				path.toString()));
+		String line = null;
+
+		try {
+			while ((line = fis.readLine()) != null) {
+				String[] words = line.split(",");
+				// movieid, release year, movie title (title may contain comma's)
+				String title = "";
+				if (words.length == 3)
+					title = words[2];
+				else if (words.length > 3) {
+					for (int i = 2; i < words.length - 1; i++)
+						title += words[i] + ",";
+					
+					title += words[words.length - 1];
+				}
+					
+				if (title == "")
+					return;
+				
+				Integer mId = Integer.parseInt(words[0]);
+				movieCache.put(mId, title);
+				yearCache.put(mId, Integer.parseInt(words[1]));
+			}
+		} finally {
+			fis.close();
+		}
+	}
 	
 	
-	public static class Map extends Mapper<NullWritable, BytesWritable, NullWritable, Text> {
+	public static class Map extends Mapper<LongWritable, Text, NullWritable, Text> {
 		
-//		private Text filename;
-//		
-//		@Override
-//		protected void  setup(Context context) throws IOException, InterruptedException {
-//			InputSplit split = context.getInputSplit();
-//			Path path = ((FileSplit) split).getPath();
-//			filename = new Text(path.toString());
-//		}
+		JaccardSimilarity jSim = new JaccardSimilarity();
+		LevenshteinDistance lDist = new LevenshteinDistance();
 		
 		StringBuilder s = new StringBuilder();
 		Text movieKey = new Text();
 		Text movieVal = new Text();
 		
-		public void map(NullWritable key, BytesWritable value, Context context)
+		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String content = new String(value.getBytes());
-//			String[] lines = content.split("\n");
-//			
-//			String movieID = (lines[0].split(":"))[0];
-//			
-//			System.out.println(String.format("Hello:%s,Length:%d", movieID, lines.length));
-//			
-//			for (int i = 1; i < lines.length - 2; i++) {
-//				movieKey.set(movieID);
-//				Text movieInfo = lines[i];
-//				context.write(movieKey, lines[i])
-//			}
 			
-			StringTokenizer tokenizer = new StringTokenizer(content);
-			String movieID = null;
-			if (tokenizer.hasMoreTokens()) {
-				movieID = (tokenizer.nextToken()).split(":")[0];
-				movieKey.set(movieID);
-			}
+			String line = value.toString();
+			String[] mvInfo = line.split("\\d{4}");
 			
-			if (movieID != null) {
-				while (tokenizer.hasMoreTokens()) {
-					String movieInfo = tokenizer.nextToken();
-					// reached last (empty) line
-					if (movieInfo == null || movieInfo.trim().length() == 0)
-						break;
-					
-					String[] words = movieInfo.split(",");
-					movieInfo = String.format("%s,%s,%s", words[0], movieID, words[1]);
-					movieVal.set(movieInfo);
-					context.write(NullWritable.get(), movieVal);
+			String movieTitle = mvInfo[0].trim();
+			String genre = mvInfo[2].trim();
+			
+			if (movieTitle != null && movieTitle != "") {
+				double min = Double.POSITIVE_INFINITY;
+				String matchedMovie = "";
+				int movieID = 0;
+				
+				for (String movie : movieCache.values()) {
+					double val = jSim.compare(movie, movieTitle);
+					if (min > val) {
+						min = val;
+						matchedMovie = movie;
+					}
 				}
+				
+				for (int mid : movieCache.keySet()) {
+					if (movieCache.containsValue(matchedMovie)) {
+						movieID = mid;
+						break;
+					}
+				}
+				
+				String output = String.format("%d,%s,%s,%s", movieID, yearCache.get(movieID), matchedMovie, genre);
+				context.write(NullWritable.get(), new Text(output));
 			}
 		}
-		
-		
-//		public void map(NullWritable key, BytesWritable value, Context context)
-//				throws IOException, InterruptedException {
-//			String content = new String(value.getBytes());
-//			String[] lines = content.split("\n");
-//			
-//			String movieID = (lines[0].split(":"))[0];
-//			
-//			System.out.println(String.format("Hello:%s,Length:%d", movieID, lines.length));
-//			
-//			//StringBuilder s = new StringBuilder();
-//			
-//			for (int i = 1; i < lines.length - 2; i++) {
-//				String[] words = lines[i].split(",");
-//				// ensure words.length = 3 (customerid, rating, date)
-//				s.append(String.format("%s,%s,%s\n", words[0], movieID, words[1]));
-//			}
-//			// process last two lines
-//			String[] lastButOne = lines[lines.length - 2].split(",");
-//			s.append(String.format("%s,%s,%s", lastButOne[0], movieID, lastButOne[1]));
-//			
-//			// incase last line is empty line in input files then do not write to output
-//			String[] last = lines[lines.length - 1].split(",");
-//			if (last != null && last.length == 3)
-//				s.append(String.format("\n%s,%s,%s", last[0], movieID, last[1]));
-//			
-//			//BytesWritable data = new BytesWritable(s.toString().getBytes());
-//			//context.write(key, data);
-//			context.write(key, new Text(s.toString()));
-			
-			
-//			String content = new String(value.getBytes());
-//			String[] lines = content.split("\n");
-//			
-//			String movieID = (lines[0].split(":"))[0];
-//			
-//			System.out.println(String.format("Hello:%s,Length:%d", movieID, lines.length));
-//			
-//			List<String> buffer = new ArrayList<String>();
-//			
-//			for (int i = 1; i < lines.length - 2; i++) {
-//				String[] words = lines[i].split(",");
-//				// ensure words.length = 3 (customerid, rating, date)
-//				buffer.add(String.format("%s,%s,%s\n", words[0], movieID, words[1]));
-//			}
-//			// process last two lines
-//			String[] lastButOne = lines[lines.length - 2].split(",");
-//			buffer.add(String.format("%s,%s,%s", lastButOne[0], movieID, lastButOne[1]));
-//			
-//			// incase last line is empty line in input files then do not write to output
-//			String[] last = lines[lines.length - 1].split(",");
-//			if (last != null && last.length == 3)
-//				buffer.add(String.format("\n%s,%s,%s", last[0], movieID, last[1]));
-//			
-//			String s = concatenateItems(buffer);
-//			
-//			//BytesWritable data = new BytesWritable(s.toString().getBytes());
-//			//context.write(key, data);
-//			context.write(key, new Text(s));
-//			
-//		}
-		
-//		public static String concatenateItems(List<String> items) {
-//			if (items == null) 
-//				return null;
-//			if (items.size() == 0) 
-//				return "";
-//			
-//			int size = 0;
-//			for (String item : items)
-//				size += item.length();
-//			
-//			StringBuilder result = new StringBuilder(size);
-//			for (String item : items)
-//				result.append(item);
-//			
-//			return result.toString();
-//		}
 	}
 	
-	public static class Reduce extends Reducer<NullWritable, Text, NullWritable, Text> {
-
-		public void reduce(NullWritable key, Iterable<Text> values, Context context)
-				throws IOException, InterruptedException {
-			for (Text value : values)
-				context.write(NullWritable.get(), value);
-		}
-	}
 	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = new Job(conf, "MergetoCSV");
+		// Sample test file
+		DistributedCache.addCacheFile(new URI(args[1]), conf);
+		Job job = new Job(conf, "GenreAddition");
 		job.setJarByClass(MergeUtility.class);
 		
 		job.setMapperClass(Map.class);
 		//job.setCombinerClass(Reduce.class);
-		job.setReducerClass(Reduce.class);
+		//job.setReducerClass(Reduce.class);
 		
-		job.setInputFormatClass(WholeFileInputFormat.class);
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(Text.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
